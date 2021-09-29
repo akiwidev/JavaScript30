3e22f4e8df2a902c72eb1018f48a4a9bd5660868
cheri kennedy <cheri.k3nn3dy@gmail.com>
2021-09-29 13:07:06 +0900

    added timeupdate event listener to video, and created handleProgress function

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -8,7 +8,7 @@ const skipButtons = player.querySelectorAll("[data-skip]");
 const ranges = player.querySelectorAll(".player__slider");
 
 
-/* build our function */
+/* build our functions */
 
 function togglePlay() {
   /* another way */
@@ -34,10 +34,16 @@ function handleRangeUpdate() {
   video[this.name] = this.value;
 }
 
+function handleProgress() {
+  const percent = (video.currentTime / video.duration) * 100;
+  progressBar.style.flexBasis = `${percent}%`
+}
+
 /* hook up the event listeners */
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
+video.addEventListener('timeupdate', handleProgress);
 toggle.addEventListener('click', togglePlay);
 skipButtons.forEach(button => button.addEventListener("click", skip));
 ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
